733975117a09e5cdb43d4259a213390ad0f1e11b
alexmost <alexmost1989@gmail.com>
2025-05-15 17:41:32 +0200

    upgrade services format

--- src/build/services.mjs
+++ src/build/services.mjs
@@ -3,31 +3,34 @@ import { parse } from "csv-parse/sync";
 import fs from "fs/promises";
 import path from "path";
 
+function recordsToServices(records) {
+  return records
+    .map((r) => r.filter((item) => Boolean(item)))
+    .reduce((acc, item) => {
+      if (item.length === 1) {
+        acc.push(item);
+      } else {
+        acc[acc.length - 1].push(item);
+      }
+      return acc;
+    }, [])
+    .map(([title, ...options]) => ({
+      title,
+      options,
+    }));
+}
+
 const sheetURL =
-  "https://docs.google.com/spreadsheets/d/1XEzx6YljRJ9p5gMkYyRPzwhVKydStVVgIVWIxKgXxHQ/gviz/tq?tqx=out:csv&sheet=services";
+  "https://docs.google.com/spreadsheets/d/1XEzx6YljRJ9p5gMkYyRPzwhVKydStVVgIVWIxKgXxHQ/gviz/tq?tqx=out:csv&sheet=sluzby";
 
 export async function fetchServices() {
   const response = await fetch(sheetURL);
   const csvText = await response.text();
 
   const records = parse(csvText);
-
-  const servicesObj = {};
-
-  records.forEach(([category, service, price]) => {
-    if (!servicesObj[category]) {
-      servicesObj[category] = [];
-    }
-    servicesObj[category].push([service, price]);
-  });
-
-  const servicesArray = Object.entries(servicesObj).map(([title, options]) => ({
-    title,
-    options,
-  }));
-
+  const services = recordsToServices(records);
   const servicesPath = path.join("./src/auto", "services.json");
-  await fs.writeFile(servicesPath, JSON.stringify(servicesArray), "utf8", {
+  await fs.writeFile(servicesPath, JSON.stringify(services), "utf8", {
     flag: "wx",
   });
 }
